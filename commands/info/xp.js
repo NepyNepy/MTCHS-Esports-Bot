1e95b4824a35f2093357e55dd5e46ea223725348
NepyNepy <fazeobject@gmail.com>
2022-02-09 08:52:36 -0700

    XP Command

---
+++ commands/info/xp.js
@@ -0,0 +1,157 @@
+module.exports = {
+    name: "xp",
+    category: "info",
+    permissions: [],
+    devOnly: false,
+    run: async ({client, message, args}) => {
+        let array = message.content.split(" ").slice(1).join(" ")
+
+        let firstValue = array
+
+        let SecondValue = Number(firstValue)
+
+        if (SecondValue > 0 && SecondValue < 101) {
+            let ThirdValue = (100 - SecondValue)
+            let FourthValue = (ThirdValue * 20000)
+            message.reply(`You have entered you are level ${SecondValue}\n\n ***XP TILL NEXT STAR: ${FourthValue}***`)
+
+        } else if (SecondValue > 100 && SecondValue < 201) {
+            let ThirdValue = (200 - SecondValue)
+            let FourthValue = (ThirdValue * 20000)
+            message.reply(`You have entered you are level ${SecondValue}\n\n ***XP TILL NEXT STAR: ${FourthValue}***`)
+
+        } else if (SecondValue > 200 && SecondValue < 301) {
+            let ThirdValue = (300 - SecondValue)
+            let FourthValue = (ThirdValue * 20000)
+            message.reply(`You have entered you are level ${SecondValue}\n\n ***XP TILL NEXT STAR: ${FourthValue}***`)
+        } else if (SecondValue > 300 && SecondValue < 401) {
+            let ThirdValue = (300 - SecondValue)
+            let FourthValue = (ThirdValue * 20000)
+            message.reply(`You have entered you are level ${SecondValue}\n\n ***XP TILL NEXT STAR: ${FourthValue}***`)
+
+        } else if (SecondValue > 400 && SecondValue < 501) {
+            let ThirdValue = (400 - SecondValue)
+            let FourthValue = (ThirdValue * 20000)
+            message.reply(`You have entered you are level ${SecondValue}\n\n ***XP TILL NEXT STAR: ${FourthValue}***`)
+
+        } else if (SecondValue > 500 && SecondValue < 601) {
+            let ThirdValue = (500 - SecondValue)
+            let FourthValue = (ThirdValue * 20000)
+            message.reply(`You have entered you are level ${SecondValue}\n\n ***XP TILL NEXT STAR: ${FourthValue}***`)
+
+        } else if (SecondValue > 600 && SecondValue < 701) {
+            let ThirdValue = (600 - SecondValue)
+            let FourthValue = (ThirdValue * 20000)
+            message.reply(`You have entered you are level ${SecondValue}\n\n ***XP TILL NEXT STAR: ${FourthValue}***`)
+
+        } else if (SecondValue > 700 && SecondValue < 801) {
+            let ThirdValue = (700 - SecondValue)
+            let FourthValue = (ThirdValue * 20000)
+            message.reply(`You have entered you are level ${SecondValue}\n\n ***XP TILL NEXT STAR: ${FourthValue}***`)
+
+        } else if (SecondValue > 800 && SecondValue < 901) {
+            let ThirdValue = (800 - SecondValue)
+            let FourthValue = (ThirdValue * 20000)
+            message.reply(`You have entered you are level ${SecondValue}\n\n ***XP TILL NEXT STAR: ${FourthValue}***`)
+
+        } else if (SecondValue > 900 && SecondValue < 1001) {
+            let ThirdValue = (900 - SecondValue)
+            let FourthValue = (ThirdValue * 20000)
+            message.reply(`You have entered you are level ${SecondValue}\n\n ***XP TILL NEXT STAR: ${FourthValue}***`)
+
+        } else if (SecondValue > 1000 && SecondValue < 1101) {
+            let ThirdValue = (1000 - SecondValue)
+            let FourthValue = (ThirdValue * 20000)
+            message.reply(`You have entered you are level ${SecondValue}\n\n ***XP TILL NEXT STAR: ${FourthValue}***`)
+
+        } else if (SecondValue > 1100 && SecondValue < 1201) {
+            let ThirdValue = (1100 - SecondValue)
+            let FourthValue = (ThirdValue * 20000)
+            message.reply(`You have entered you are level ${SecondValue}\n\n ***XP TILL NEXT STAR: ${FourthValue}***`)
+
+        } else if (SecondValue > 1200 && SecondValue < 1301) {
+            let ThirdValue = (1200 - SecondValue)
+            let FourthValue = (ThirdValue * 20000)
+            message.reply(`You have entered you are level ${SecondValue}\n\n ***XP TILL NEXT STAR: ${FourthValue}***`)
+
+        } else if (SecondValue > 1300 && SecondValue < 1401) {
+            let ThirdValue = (1300 - SecondValue)
+            let FourthValue = (ThirdValue * 20000)
+            message.reply(`You have entered you are level ${SecondValue}\n\n ***XP TILL NEXT STAR: ${FourthValue}***`)
+
+        } else if (SecondValue > 1400 && SecondValue < 1501) {
+            let ThirdValue = (1400 - SecondValue)
+            let FourthValue = (ThirdValue * 20000)
+            message.reply(`You have entered you are level ${SecondValue}\n\n ***XP TILL NEXT STAR: ${FourthValue}***`)
+
+        } else if (SecondValue > 1500 && SecondValue < 1601) {
+            let ThirdValue = (1500 - SecondValue)
+            let FourthValue = (ThirdValue * 20000)
+            message.reply(`You have entered you are level ${SecondValue}\n\n ***XP TILL NEXT STAR: ${FourthValue}***`)
+
+        } else if (SecondValue > 1600 && SecondValue < 1701) {
+            let ThirdValue = (1600 - SecondValue)
+            let FourthValue = (ThirdValue * 20000)
+            message.reply(`You have entered you are level ${SecondValue}\n\n ***XP TILL NEXT STAR: ${FourthValue}***`)
+
+        } else if (SecondValue > 1700 && SecondValue < 1801) {
+            let ThirdValue = (1700 - SecondValue)
+            let FourthValue = (ThirdValue * 20000)
+            message.reply(`You have entered you are level ${SecondValue}\n\n ***XP TILL NEXT STAR: ${FourthValue}***`)
+
+        } else if (SecondValue > 1800 && SecondValue < 1901) {
+            let ThirdValue = (1800 - SecondValue)
+            let FourthValue = (ThirdValue * 20000)
+            message.reply(`You have entered you are level ${SecondValue}\n\n ***XP TILL NEXT STAR: ${FourthValue}***`)
+
+        } else if (SecondValue > 1900 && SecondValue < 2001) {
+            let ThirdValue = (1900 - SecondValue)
+            let FourthValue = (ThirdValue * 20000)
+            message.reply(`You have entered you are level ${SecondValue}\n\n ***XP TILL NEXT STAR: ${FourthValue}***`)
+
+        } else if (SecondValue > 2000 && SecondValue < 2101) {
+            let ThirdValue = (2000 - SecondValue)
+            let FourthValue = (ThirdValue * 20000)
+            message.reply(`You have entered you are level ${SecondValue}\n\n ***XP TILL NEXT STAR: ${FourthValue}***`)
+
+        } else if (SecondValue > 2100 && SecondValue < 2201) {
+            let ThirdValue = (2100 - SecondValue)
+            let FourthValue = (ThirdValue * 20000)
+            message.reply(`You have entered you are level ${SecondValue}\n\n ***XP TILL NEXT STAR: ${FourthValue}***`)
+
+        } else if (SecondValue > 2200 && SecondValue < 2301) {
+            let ThirdValue = (2200 - SecondValue)
+            let FourthValue = (ThirdValue * 20000)
+            message.reply(`You have entered you are level ${SecondValue}\n\n ***XP TILL NEXT STAR: ${FourthValue}***`)
+
+        } else if (SecondValue > 2300 && SecondValue < 2401) {
+            let ThirdValue = (2300 - SecondValue)
+            let FourthValue = (ThirdValue * 20000)
+            message.reply(`You have entered you are level ${SecondValue}\n\n ***XP TILL NEXT STAR: ${FourthValue}***`)
+        } else if (SecondValue > 2400 && SecondValue < 2501) {
+            let ThirdValue = (2400 - SecondValue)
+            let FourthValue = (ThirdValue * 20000)
+            message.reply(`You have entered you are level ${SecondValue}\n\n ***XP TILL NEXT STAR: ${FourthValue}***`)
+        } else if (SecondValue > 2500 && SecondValue < 2601) {
+            let ThirdValue = (2500 - SecondValue)
+            let FourthValue = (ThirdValue * 20000)
+            message.reply(`You have entered you are level ${SecondValue}\n\n ***XP TILL NEXT STAR: ${FourthValue}***`)
+        } else if (SecondValue > 2600 && SecondValue < 2701) {
+            let ThirdValue = (2600 - SecondValue)
+            let FourthValue = (ThirdValue * 20000)
+            message.reply(`You have entered you are level ${SecondValue}\n\n ***XP TILL NEXT STAR: ${FourthValue}***`)
+        } else if (SecondValue > 2700 && SecondValue < 2801) {
+            let ThirdValue = (2700 - SecondValue)
+            let FourthValue = (ThirdValue * 20000)
+            message.reply(`You have entered you are level ${SecondValue}\n\n ***XP TILL NEXT STAR: ${FourthValue}***`)
+        } else if (SecondValue > 2800 && SecondValue < 2901) {
+            let ThirdValue = (2800 - SecondValue)
+            let FourthValue = (ThirdValue * 20000)
+            message.reply(`You have entered you are level ${SecondValue}\n\n ***XP TILL LAST STAR: ${FourthValue}***`)
+        } else {
+            message.reply(`Number entered is out of range! Please try again with a valid number.`)
+        }
+        
+    }
+
+}
